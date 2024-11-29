20c6ebdeee2b5f183b91a92f3dd43d960ad0f9f3
Rafael Magno <rafaelmagno125@gmail.com>
2024-11-29 18:50:01 -0300

    chore: ajuste de arquivo .gitignore

--- obj/Debug/net8.0/api-bookStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-bookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-bookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad715b8a543fd8432a80d151d6fc944bb952c061")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c1507ff5e8636de889aa451e9464634d8b8b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-bookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-bookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
